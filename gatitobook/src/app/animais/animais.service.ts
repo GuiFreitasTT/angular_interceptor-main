0034dbbf510292ab58e1dc225dc3ea0b5aea2a4b
GuiFreitasTT <guidiegofreitas@yahoo.com.br>
2022-12-15 14:32:50 -0300

    last commit

--- gatitobook/src/app/animais/animais.service.ts
+++ gatitobook/src/app/animais/animais.service.ts
@@ -3,7 +3,7 @@ import { TokenService } from './../autenticacao/token.service';
 import { Observable } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Animais } from './animais';
+import { Animais, Animal } from './animais';
 
 const API = environment.apiURL;
 
@@ -15,10 +15,10 @@ export class AnimaisService {
   constructor(private http:HttpClient, private tokenService: TokenService) { }
 
   listaDoUsuario(nomeDoUsuario:string):Observable<Animais>{
-    const token = this.tokenService.retornaToken();
-    const headers = new HttpHeaders().append('x-acess-token', token);
-    return this.http.get<Animais>(`${API}/${nomeDoUsuario}/photos`, {
-      headers,
-    });
+    return this.http.get<Animais>(`${API}/${nomeDoUsuario}/photos`);
+  }
+
+  buscaPorID(id: number): Observable<Animal>{
+    return this.http.get<Animal>(`${API}/photos/${id}`);
   }
 }
